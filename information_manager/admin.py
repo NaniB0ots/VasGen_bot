1f1b9b7c47417297a1f05f227ad117a6cd6222d6
bolanebyla <alexleskov2000@gmail.com>
2021-02-22 18:38:46 +0800

    Создать модель для новостей

--- information_manager/admin.py
+++ information_manager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from information_manager import models
+
+
+@admin.register(models.News)
+class TgUserAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title', 'news',)
+    list_filter = ('author', 'tags')
+    ordering = ['-update_date']
+
+
+admin.site.register(models.Tag)
